4bc6a98223d5922268e8297dd331777741b78427
Chelsea Yue <cyue@lcs.on.ca>
2019-11-13 19:18:01 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Yue, Chelsea on 2019/11/13.
+//  Copyright © 2019 Chelsea Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
